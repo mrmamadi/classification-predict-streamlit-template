5b9e4149a36d254d5450d22623f63c9126ac113f
BNkosi <bulelaninkosi9@gmail.com>
2020-06-27 16:42:27 +0200

    debugged functions

--- eda.py
+++ eda.py
@@ -28,11 +28,11 @@ from nltk.stem.wordnet import WordNetLemmatizer
 from textblob import TextBlob
 
 # Import datasets
-train_data = pd.read_csv('/resources/datasets/train.csv')
+# train_data = pd.read_csv('/resources/datasets/train.csv')
 
 
 # Functions
-def getVocab(df=train_data):
+def getVocab(df):
     """
     count total vocabulary from Dataframe message
     
@@ -52,7 +52,7 @@ def getVocab(df=train_data):
             vocab.append(token)
     return vocab
 
-def wordFrequencyDict(df = train_data, target='target', vocab = getVocab()):
+def wordFrequencyDict(df, target, vocab):
     """
     function that returns dictionary of word frequencies from a dataframe
     Parameters
@@ -72,11 +72,11 @@ def wordFrequencyDict(df = train_data, target='target', vocab = getVocab()):
         ('drug', 0.004),
          ('validated', 0.004)]
     """
-    data=df.copy()
+
     word_frequency_dict = {}
     for label in df[target].unique():
         data = df[df[target] == label]
-        class_vocab = vocab
+        class_vocab = getVocab(data)
         length_of_vocab = len(class_vocab)
         ordered_class_words = Counter(class_vocab).most_common()
         ordered_class_words_freq = list()
@@ -87,7 +87,7 @@ def wordFrequencyDict(df = train_data, target='target', vocab = getVocab()):
             word_frequency_dict[label] = ordered_class_words_freq
     return word_frequency_dict
 
-def getClassWords(word_frequency_dict = wordFrequencyDict()):
+def getClassWords(word_frequency_dict):
     """
     doctstring
     Parameters
@@ -113,7 +113,7 @@ def getClassWords(word_frequency_dict = wordFrequencyDict()):
         class_words[label] = words
     return class_words
 
-def getOrder(class_words = getClassWords()):
+def getOrder(class_words, df):
     """
     
     """
@@ -125,7 +125,7 @@ def getOrder(class_words = getClassWords()):
     label_specific_words = dict(
         Pro = pro_spec_words, Neutral = neutral_spec_words, Anti = anti_spec_words, News = news_spec_words
         )
-
+    vocab = getVocab(df)
 
     class_specific_words = pro_spec_words + neutral_spec_words + anti_spec_words + news_spec_words
     class_specific_words[:5], len(class_specific_words)
@@ -133,7 +133,7 @@ def getOrder(class_words = getClassWords()):
     ordered_words = Counter(vocab).most_common()
     return ordered_words
 
-def topNWords(ordered_words = getOrder(), n = 5000):
+def topNWords(ordered_words, n = 5000):
     """
     count total vocabulary from Dataframe message
     
@@ -149,7 +149,7 @@ def topNWords(ordered_words = getOrder(), n = 5000):
         most_common.append(word[0])
     return most_common
 
-def removeInfrequentWords(tweet, include = topNWords()):
+def removeInfrequentWords(tweet, include):
     """
     Function that goes through the words in a tweet,
     determines if there are any words that are not in
@@ -179,17 +179,16 @@ def removeInfrequentWords(tweet, include = topNWords()):
             filt_tweet.append(token)
     return filt_tweet
 
-def allVocab(df=train_data, target = 'tweets_clean'):
+def allVocab(df, target = 'tweets_clean'):
     all_vocab = list()
     for tweet in df[target]:
         for token in tweet:
             all_vocab.append(token)
     return all_vocab
 
-def removeCommonWords(tweet):
+def removeCommonWords(tweet, very_common_words):
     """
     removes the most common words from a list of given words
-    
     Parameters
     ----------
         tweet (list): list of words to be cleaned
@@ -223,11 +222,11 @@ def lengthOfTweet(tweet):
     length = len(tweet)
     return length
 
-def plotDist(df=train_data, target = 'len_of_tweet'):
-    sns.distplot(train_data['len_of_tweet'])
+def plotDist(df, target = 'len_of_tweet'):
+    sns.distplot(df[target])
     plt.show()
 
-def plotCounts(df=train_data):
+def plotCounts(df):
     sns.countplot(data = df, x = 'target', palette = {'Pro':'#CCCC00', 'News':'teal', 'Neutral':'teal', 'Anti':'teal'})
     plt.title('Count of Sentiments\n')
     plt.xlabel('\nSentiment')
@@ -262,7 +261,7 @@ def plotWordCloud(data, label):
     wordcloud = WordCloud(contour_width=3, contour_color='steelblue').generate(words)
 
     # Display the generated image:
-    fig = plt.figure(figsize = (10, 6))
+    plt.figure(figsize = (10, 6))
     plt.imshow(wordcloud, interpolation='bilinear')
     plt.title(label, fontsize = 30)
     plt.axis("off")
@@ -289,7 +288,7 @@ def getPolarityScores(tweet):
     # return the polarity scores
     return scores
 
-def applyScores(data = train_data):
+def applyScores(data):
     nltk_scores = dict(compound = list(), negative = list(), neutral = list(), positive = list())
     for tweet in data['tweets_clean']:
         output = getPolarityScores(tweet)
@@ -311,7 +310,7 @@ def applyScores(data = train_data):
         data = pd.concat([data, pd.DataFrame(nltk_scores)], axis = 1)
     return data
 
-def getPolaritySubjectivity(data = train_data):
+def getPolaritySubjectivity(data):
     sentiment_scores = [TextBlob(' '.join(tweet)).sentiment for tweet in data['tweets_clean']]
 
     # Add output to dataframe
@@ -325,7 +324,7 @@ def getPolaritySubjectivity(data = train_data):
     data['subjectivity'] = subj
     return data
 
-def violinPlots(data=train_data, target = 'target'):
+def violinPlots(data, target = 'target'):
     fig, axes = plt.subplots(1, 3, figsize = (18, 5))
     for i, column in enumerate(['compound', 'polarity', 'subjectivity']):
         g = sns.violinplot(data = data, target = 'target', y = column, ax = axes[i], palette = {'Pro':'#CCCC00', 'News':'teal', 'Neutral':'teal', 'Anti':'teal'})
@@ -336,6 +335,7 @@ def violinPlots(data=train_data, target = 'target'):
             g.set_ylabel(' ')
         g.set_xlabel(' ')
     plt.show()
+    return fig
 
 def plotScatter(x, y, df, title):
     """
@@ -345,49 +345,49 @@ def plotScatter(x, y, df, title):
     ----------
         x (str): variable string from dataframe
         y (str): variable string from dataframe
-        df (DataFrame): input of dataframe
+        dict (dict): input of word list
         
     Returns
     -------
         g (plot): display a scatter plot with points of each labelled sentiments
     
     """
-    fig = plt.figure(figsize = (8, 5))
-    g = sns.scatterplot(data = df, x = x, y = y, hue = 'target', legend = False, palette = {'Pro':'#CCCC00', 'News':'teal', 'Neutral':'teal', 'Anti':'teal'})
-    g.set_title(title, fontsize = 20)
+    plt.figure(figsize = (8, 5))
+    sns.scatterplot(data = df, x = x, y = y, hue = 'target', legend = False, palette = {'Pro':'#CCCC00', 'News':'teal', 'Neutral':'teal', 'Anti':'teal'})
+    plt.title(title, fontsize = 20)
     
     # add annotations one by one with a loop
-    for line in range(0,data.shape[0]):
-        g.text(data[x][line], data[y][line], data['target'][line], 
+    for line in range(0,df.shape[0]):
+        g.text(df[x][line], df[y][line], df['target'][line], 
                 horizontalalignment='left', size='large', color='black')
     
     return g
 
-def plotAltScatter(df = train_data, X = 'compound', y_ = 'polarity', title_ = 'Compound Vs Polarity\n'):
+def plotAltScatter(df, X = 'compound', y_ = 'polarity', title_ = 'Compound Vs Polarity\n'):
     data = df.groupby('target')[['negative', 'positive', 'neutral', 'compound', 'polarity', 'subjectivity']].mean().reset_index()
     plotScatter(x = X, y = y_, df = data, title = title_)
     plt.xlabel('\nCompound Score')
     plt.ylabel('Polarity\n')
     plt.show()
 
-def arrowScatter(df = train_data):
-    fig = plt.figure(figsize = (8, 5))
-    g = sns.scatterplot(data = df, x = 'subjectivity', y = 'polarity', color = 'teal', hue = 'target', alpha = 1/3)
-    g.arrow(0, 0.1, 0.99, 1, fc = 'black', ec = '#CCCC00')
-    g.arrow(0, 0.1, 0.99, -1, fc = 'black', ec = '#CCCC00')
+def arrowScatter(df):
+    plt.figure(figsize = (8, 5))
+    sns.scatterplot(data = df, x = 'subjectivity', y = 'polarity', color = 'teal', hue = 'target', alpha = 1/3)
+    plt.arrow(0, 0.1, 0.99, 1, fc = 'black', ec = '#CCCC00')
+    plt.arrow(0, 0.1, 0.99, -1, fc = 'black', ec = '#CCCC00')
     plt.title('Subjectiviy vs Polarity\n')
     plt.xlabel('\nSubjectivity')
     plt.ylabel('Polarity')
     plt.show()
 
-def histPlot(df = train_data):
+def histPlot(df):
     columns = ['polarity', 'compound']
     fig, axes = plt.subplots(1, len(columns), figsize = (18, 5), sharey = True)
     for i, column in enumerate(columns):
         sns.distplot(df[column], ax = axes[i])
     plt.show()
 
-def polCompNeutralPlot(df = train_data):
+def polCompNeutralPlot(df):
     # variables of columns from dataframe that are conditioned to zero
     polarity_mask = df['polarity'] == 0
     compound_mask = df['compound'] == 0
@@ -397,15 +397,19 @@ def polCompNeutralPlot(df = train_data):
     sns.distplot(sum_of_pol_and_comp)
     plt.show()
 
-def polPlusCompScatter(df = train_data):
+def polPlusCompScatter(df):
     data = df.groupby('target')[['pol_plus_comp', 'subjectivity']].mean().reset_index()
     plotScatter(x = 'subjectivity', y = 'pol_plus_comp', df = data, title="Polarity & Compound vs Subjectivity")
     plt.show()
 
-def midCloud(df = train_data, lower = -0.15, inteval = 0.3, sentiment = [-1,0,1,2]):
-    data = df[(df['sentiment']==sentiment) & (df['compound'] < lower+interval) & (df['compound'] > lower)]
+def midCloud(df, lower = -0.15, interval = 0.3, sentiment = [-1,0,1,2]):
+    data = df[(df['sentiment']==sentiment) & (df['compound'] < lower + interval) & (df['compound'] > lower)]
     plotWordCloud(data, label = f'Neutral where: {lower} < Compound < {lower+interval}')
 
-def upperCloud(df = train_data, upper = 0.6, inteval = 0.3, sentiment = [-1,0,1,2]):
+def upperCloud(df, upper = 0.6, sentiment = [-1,0,1,2]):
     data = df[(df['sentiment']==sentiment)&(df['compound'] > upper)]
     plotWordCloud(data, label = f'compound > {upper}')
+
+def lowerCloud(df, lower = -0.6, sentiment = [-1,0,1,2]):
+    data = df[(df['sentiment']==sentiment)&(df['compound'] > lower)]
+    plotWordCloud(data, label = f'compound > {lower}')
